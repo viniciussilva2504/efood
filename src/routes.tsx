e0a862cd579974f39993cb5c0c43681bc2bdd814
viniciussilva2504 <vinicius.silva2504@gmail.com>
2025-09-18 17:23:34 +0100

    fix3

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Perfil from './pages/Perfil'
-import Test from './pages/Test'
 
 const Rotas = () => (
   <Routes>
-    <Route path="/" element={<Test />} />
-    <Route path="/home" element={<Home />} />
+    <Route path="/" element={<Home />} />
     <Route path="/perfil/:id" element={<Perfil />} />
   </Routes>
 )
