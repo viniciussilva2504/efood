4993c8138bdba37bf3fcde3590a8964722e7b5e4
Vinic <vinicius.silva2504@gmail.com>
2026-04-20 15:07:39 +0100

    fix: kill service worker - self-destruct to clear all cached content

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,41 +1,10 @@
-const CACHE_NAME = 'efood-cache-v2'
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-]
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting()
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache)
-    }),
-  )
-})
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const responseClone = response.clone()
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone)
-        })
-        return response
-      })
-      .catch(() => caches.match(event.request)),
-  )
-})
-
+// Self-destruct: unregister this service worker and clear all caches
+self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName)),
-      )
-    }).then(() => self.clients.claim()),
+    caches.keys().then((names) =>
+      Promise.all(names.map((name) => caches.delete(name)))
+    ).then(() => self.clients.claim())
+    .then(() => self.registration.unregister())
   )
 })
