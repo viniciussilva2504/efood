f7efc73eb96e3116e38ca8661c01ffb40b8a3b82
Vinic <vinicius.silva2504@gmail.com>
2026-04-20 15:52:09 +0100

    chore: cleanup - remove debug files, consolidate styles, translate metadata

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,10 @@
 self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(names.map((name) => caches.delete(name)))
-    ).then(() => self.clients.claim())
-    .then(() => self.registration.unregister())
+    caches
+      .keys()
+      .then((names) => Promise.all(names.map((name) => caches.delete(name))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister()),
   )
 })
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 
 export const cores = {
   begeclaro: '#FFF8F2',
@@ -6,21 +6,6 @@ export const cores = {
   rosa: '#E66767',
   branco: '#FFFFFF'
 }
-
-export const GlobalCss = createGlobalStyle`
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    text-decoration: none;
-    font-family: 'Roboto', sans-serif;
-  }
-
-  body {
-    background-color: ${cores.begeclaro};
-  }
-`
 
 export const Container = styled.div`
   max-width: 1024px;
